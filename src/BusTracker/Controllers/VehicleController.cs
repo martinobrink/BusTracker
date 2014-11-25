91d7553f379f8ec8a57bcaff4cf0fb7853b26b4b
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2014-11-25 15:17:48 +0100

    Added Vehicle model and Controller classes

---
+++ src/BusTracker/Controllers/VehicleController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BusTracker.Models;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BusTracker.Controllers.Controllers
+{
+    [Route("api/[controller]")]
+    public class VehicleController : Controller
+    {
+
+        static readonly List<Vehicle> _vehicles = new List<Vehicle>()
+        {
+            new Vehicle
+            {
+                Id = "123456",
+                Name = "4A",
+                Description = "Sejeste bus i verden..."
+            }
+        };
+
+        [HttpGet]
+        public IEnumerable<Vehicle> Get()
+        {
+            return _vehicles;
+        }
+
+        [HttpGet("{id}")]
+        public Vehicle Get(string id)
+        {
+            return _vehicles.FirstOrDefault( x => x.Id == id);
+        }
+
+        [HttpPut()]
+        public IActionResult Put([FromBody]Vehicle vehicle)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new HttpStatusCodeResult(400);
+            }
+
+            if (!String.IsNullOrEmpty(vehicle.Id))
+            {
+                var vehicleToUpdate = _vehicles.Single(x => x.Id == vehicle.Id);
+                vehicleToUpdate.Name = vehicle.Name;
+                vehicleToUpdate.Description = vehicle.Description;
+                return new JsonResult(vehicleToUpdate);
+            }
+            else
+            {
+                var vehicleToCreate = new Vehicle
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = vehicle.Name,
+                    Description = vehicle.Description
+                };
+                _vehicles.Add(vehicleToCreate);
+                return new JsonResult(vehicleToCreate);
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete()]
+        public void DeleteAll()
+        {
+            _vehicles.Clear();
+        }
+    }
+}
---
+++ src/BusTracker/Models/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BusTracker.Models
+{
+    public class Vehicle
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
