31d1565548c35ce74ad608d63e238a8eb0ea4774
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2014-11-25 13:30:22 +0100

    Initial version for extremely naive registration of vehicles

---
+++ src/BusTracker/Controllers/VehicleController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BusTracker.Models;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BusTracker.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class VehicleController : Controller
+    {
+        static readonly List<Vehicle> _items = new List<Vehicle>()
+        {
+            new Vehicle
+            {
+                Id = "123456", 
+                Heading = "240 degrees",
+                Latitude = "12.983749857",
+                Longitude = "11.42136574561"
+            }
+        };
+
+        [HttpGet]
+        public IEnumerable<Vehicle> GetAll()
+        {
+            return _items;
+        }
+
+        [HttpPut]
+        public IActionResult CreateVehicle([FromBody] Vehicle item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new HttpStatusCodeResult(400);
+            }
+
+            _items.Add(item);
+            return new HttpStatusCodeResult(200);
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteItem(string id)
+        {
+            var item = _items.FirstOrDefault(x => x.Id == id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            _items.Remove(item);
+            return new HttpStatusCodeResult(204); // 201 No Content
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteAll()
+        {
+            _items.Clear();
+            return new HttpStatusCodeResult(204); // 201 No Content
+        }
+    }
+}
---
+++ src/BusTracker/Models/Vehicle.cs
@@ -0,0 +1,10 @@
+﻿namespace BusTracker.Models
+{
+    public class Vehicle
+    {
+        public string Id { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string Heading { get; set; }
+    }
+}
