ac93b7f8c38a20d5aa3063c8f21cefc01c7717a3
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2014-11-26 13:10:04 +0100

    re-added data seed to MemoryDb

--- src/BusTracker/MemoryDb.cs
+++ src/BusTracker/MemoryDb.cs
@@ -7,7 +7,27 @@ namespace BusTracker
 {
     public static class MemoryDb
     {
-        public static readonly Dictionary<string, Tracking> Trackings = new Dictionary<string, Tracking>();
-        public static readonly List<Vehicle> Vehicles = new List<Vehicle>();
+        public static readonly Dictionary<string, Tracking> Trackings = new Dictionary<string, Tracking>(){
+            {
+                "123456", new Tracking
+                {
+                    VehicleId = "123456",
+                    Heading = "240 degrees",
+                    Latitude = "12.983749857",
+                    Longitude = "11.42136574561",
+                    Timestamp = DateTime.UtcNow
+                }
+            }
+        };
+
+        public static readonly List<Vehicle> Vehicles = new List<Vehicle>(){
+            new Vehicle
+            {
+                Id = "123456",
+                Name = "4A",
+                Description = "Sejeste bus i verden..."
+            }
+        };
+
     }
 }
